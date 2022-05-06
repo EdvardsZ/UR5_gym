f2f9150168621fcc35d1ebdf94d2499598eb2d27
Arturs Simkuns <arturs.simkuns@edi.lv>
2022-05-06 14:23:46 +0300

    Training changes to 6000000steps

--- tests/FetchPickAndPlaceExample_train.py
+++ tests/FetchPickAndPlaceExample_train.py
@@ -19,7 +19,7 @@ model = algorithm(
     verbose = 1,
     tensorboard_log = log_path,
     gamma= 0.95,
-    learning_starts= 100000,
+    learning_starts= 10000,
     replay_buffer_class = HerReplayBuffer,
     # Parameters for HER
     replay_buffer_kwargs=dict(
